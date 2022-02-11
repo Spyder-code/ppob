adc9bc2f1c6f294b2670417e274a04bcada4b8f3
Spyder-code <luaysyauqillah@gmail.com>
2022-02-11 15:10:18 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,40 +8,40 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', function(){
-    $user = User::all()->where('role','outlet');
-    foreach ($user as $item ) {
+// Route::get('/', function(){
+//     $user = User::all()->where('role','outlet');
+//     foreach ($user as $item ) {
 
-        for ($i=0; $i < 5; $i++) {
-            $rand = rand(1,3);
-            if($rand == 1){
-                $saldo = 300000000;
-            }else if($rand == 2){
-                $saldo = 200000000;
-            }else{
-                $saldo = 100000000;
-            }
-            RiwayatSaldo::create([
-                'user_id' => $item->id,
-                'saldoAfter' => $item->saldo,
-                'saldoPlus' => $saldo,
-                'saldoNow' => $item->saldo + $saldo,
-                'created_at' => '2021-12-0'.$i.' 20:20:41',
-            ]);
+//         for ($i=0; $i < 5; $i++) {
+//             $rand = rand(1,3);
+//             if($rand == 1){
+//                 $saldo = 300000000;
+//             }else if($rand == 2){
+//                 $saldo = 200000000;
+//             }else{
+//                 $saldo = 100000000;
+//             }
+//             RiwayatSaldo::create([
+//                 'user_id' => $item->id,
+//                 'saldoAfter' => $item->saldo,
+//                 'saldoPlus' => $saldo,
+//                 'saldoNow' => $item->saldo + $saldo,
+//                 'created_at' => '2021-12-0'.$i.' 20:20:41',
+//             ]);
 
-            $total = RiwayatSaldo::where('user_id',$item->id)->sum('saldoPlus');
-            $item->update([
-                'saldo' => $total,
-            ]);
-        }
-    }
-});
+//             $total = RiwayatSaldo::where('user_id',$item->id)->sum('saldoPlus');
+//             $item->update([
+//                 'saldo' => $total,
+//             ]);
+//         }
+//     }
+// });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => ['auth:web']], function () {
 
-    // Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('landingpage');
+    Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('landingpage');
     Route::get('/riwayat', [App\Http\Controllers\HomeController::class, 'history'])->name('history');
 
     Route::get('/pulsa', [App\Http\Controllers\PulsaController::class, 'index']);
